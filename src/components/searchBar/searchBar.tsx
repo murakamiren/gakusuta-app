1de8d27809c5aaf0ef7bd44cf732cc646fc23227
renmurakami <autoeyy@gmail.com>
2022-09-10 12:55:04 +0900

    input icon, placeholder

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,10 +1,26 @@
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 import { SearchBarProps } from "./type";
+import SettingIcon from "../../assets/setting.svg";
+import SearchIcon from "../../assets/search.svg";
 
 const SearchBar: FC<SearchBarProps> = ({ placeholder }) => {
+	const [isFocus, setIsFocus] = useState<boolean>(false);
+
 	return (
-		<div className="w-full">
-			<input type="text" className="w-9/12 h-14 rounded-lg" placeholder={placeholder} />
+		<div className="w-full flex items-center justify-between">
+			<label className="w-full relative">
+				<input
+					type="text"
+					className="w-full h-14 rounded-lg px-[18px] text-primary-black"
+					onFocus={() => setIsFocus(true)}
+					onBlur={() => setIsFocus(false)}
+				/>
+				<div className={isFocus ? `opacity-0` : `opacity-100`}>
+					<p className="absolute top-1/2 left-[46px] -translate-y-1/2 text-primary-gray">{placeholder}</p>
+					<SearchIcon className="stroke-primary-gray fill-transparent absolute top-1/2 left-[18px] -translate-y-1/2" />
+				</div>
+			</label>
+			<SettingIcon className="fill-white ml-[14px]" />
 		</div>
 	);
 };
