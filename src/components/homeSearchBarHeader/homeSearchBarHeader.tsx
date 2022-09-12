c9fc7cd457aafcc9e2e5078c9405cb75e7494c64
renmurakami <autoeyy@gmail.com>
2022-09-12 17:17:28 +0900

    sticky header

--- src/components/homeSearchBarHeader/homeSearchBarHeader.tsx
+++ src/components/homeSearchBarHeader/homeSearchBarHeader.tsx
@@ -7,10 +7,10 @@ const HomeSearchBarHeader: FC = () => {
 	const router = useRouter();
 
 	return (
-		<header className="relative">
-			<div className="max-w-screen h-[225px] bg-primary-blue flex flex-col items-center pt-[44px] bg-searchBarHeaderPattern -z-10 bg-[length:170%] bg-blend-hard-light bg-[center_top_10px]">
-				<Logo className="mb-[22px] z-10" />
-				<div className="w-full px-6">
+		<header className="w-full sticky -top-[115px] z-50">
+			<div className="w-full h-[230px] bg-primary-blue flex flex-col items-center pt-[44px] bg-searchBarHeaderPattern -z-10 bg-[length:170%] bg-blend-hard-light bg-[center_top_11px]">
+				<Logo className="mb-[32px] z-10" />
+				<div className="w-full h-auto px-6 sticky top-0 flex items-center">
 					<SearchBar placeholder="エリア名で検索する" onClick={() => router.push("/search")} />
 				</div>
 			</div>
